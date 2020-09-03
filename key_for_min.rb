01c358fb297aea683316fc050b86aa7ac5425ef2
friendly-machine-4891 <liamhanafee@gmail.com>
2020-09-03 14:37:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 lowest_key = nil 
 lowest_value = 100
 hash.each_key do |key, value|
-if lowest_value == nil || 
+if lowest_value == nil || value < lowest_value
   lowest_value = value
 end
 return lowest_key
